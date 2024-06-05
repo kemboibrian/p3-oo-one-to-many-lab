4338f7197e384579314f72455baf077686ec0585
Brian Kemboi <brnkemboi@gmail.com>
2024-06-05 11:18:22 +0300

    complete

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,6 +1,6 @@
 class Pet:
     PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
-    all_pets = []
+    all = []
 
     def __init__(self, name, pet_type, owner=None):
         if pet_type not in self.PET_TYPES:
@@ -8,7 +8,9 @@ class Pet:
         self.name = name
         self.pet_type = pet_type
         self.owner = owner
-        self.__class__.all_pets.append(self)
+        self.__class__.all.append(self)
+        if owner:  # If owner is provided, add this pet to owner's pets
+            owner.add_pet(self)  # Add the pet to the owner
 
 class Owner:
     def __init__(self, name):
@@ -21,8 +23,8 @@ class Owner:
     def add_pet(self, pet):
         if not isinstance(pet, Pet):
             raise Exception("Must be a valid pet object")
-        self.owned_pets.append(pet)
-        pet.owner = self
+        self.owned_pets.append(pet)  # Append the pet object to the owned_pets list
+        pet.owner = self  # Ensure that the owner of the pet is correctly set
 
     def get_sorted_pets(self):
         sorted_pets = sorted(self.owned_pets, key=lambda x: x.name)
